1f0ee8e7b3749a7ae9febf7ab08cda2fe9dd21ba
Nicholas Bradley <nicholas.omondi@student.moringaschool.com>
2021-08-08 13:20:34 +0300

    Source class

---
+++ app/models/news_source.py
@@ -0,0 +1,9 @@
+class SourcesNews:
+
+    def __init__(self, id, name, description, url, language, category):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.language = language
+        self.category = category
