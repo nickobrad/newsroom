83ee1318d329c1d3244d9f4fefedf200160c7061
Nicholas Bradley <nicholas.omondi@student.moringaschool.com>
2021-08-08 13:19:29 +0300

    http link modified

--- app/config.py
+++ app/config.py
@@ -3,9 +3,9 @@ class Config:
     General configuration parent class
     '''
     NEWS_API_GENERAL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
+    NEWS_API_SOURCE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     NEWS_API_SOURCE = 'https://newsapi.org/v2/top-headlines/sources?category={}&apiKey={}'
-    # https://newsapi.org/v2/top-headlines/sources?category=businessapiKey=eaaf5f4dcffb435c935d12b7bd2428cc
+    NEWS_API_SEARCH = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
 
 class DevConfig(Config):
     '''
